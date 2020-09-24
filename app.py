3fc073032736d3971e6796d728b9951f0d13b51a
Nasir Mustapha <teboy.com@gmail.com>
2020-09-24 13:40:41 +0100

    added fancy html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session, redirect
+from flask import Flask, request, session, redirect, render_template, flash
 
 import random
 
@@ -21,8 +21,9 @@ def start():
     question = f'What is the capital of {state}?'
     if 'score' not in session:
         session['score'] = 0
-    html = f"""<p>{question}<p> <form method='POST' action='/submit'><input type='text' name='answer' placeholder='your answer'> <input name='state' value='{state}' hidden> <br> <input type='submit'> <p>Score: {session['score']}</p></form>"""
-    return html
+    # html = f"""<p>{question}<p> <form method='POST' action='/submit'><input type='text' name='answer' placeholder='your answer'> <input name='state' value='{state}' hidden> <br> <input type='submit'> <p>Score: {session['score']}</p></form>"""
+    # return html
+    return render_template('start.html', state=state)
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -30,9 +31,11 @@ def submit():
     state = request.form.get('state')
     if answer.lower() == session['states'][state].lower():
         session['score']+=1
+        flash('correct')
     else:
         session['score']-=1
+        flash([state, session['states'][state]])
     return redirect('start')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port='5553')
